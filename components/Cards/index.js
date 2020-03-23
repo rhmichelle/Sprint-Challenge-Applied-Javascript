008029fe35e02d9b8832afb82e4593f4a90f9c47
Michelle Rodriguez Harrison <dev@michellerh.com>
2020-03-23 00:08:29 -0400

    Didn't finish step 3

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,40 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    console.log('Bootstrap Here', response.data.articles.bootstrap)
+    // console.log(response.data.articles.bootstrap[0].headline)
+
+    // response.data.articles.bootstrap.forEach(articles => {
+    //     const newBootstrapArticle = articleComponent(articles)
+    //     cardsContainer.appendChild(newBootstrapArticle)
+    // })
+})
+    .then(response => {
+        console.log('Javascript Here', response.data.articles.javascript)
+    })
+
+
+const cardsContainer = document.querySelector('.cards-container')
+
+const articleComponent = (articles) => {
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgContainer = document.createElement('div')
+    const img = document.createElement('img')
+    const authorsName = document.createElement('span')
+
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    headline.textContent = articles.headline
+    img.src = articles.authorPhoto
+    authorsName.textContent = articles.authorName
+
+    return card
+}
